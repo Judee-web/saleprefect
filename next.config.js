ff798b0d62de11fedf72f73ccffe40322d84c2bc
Jude <ebidedabofa@gmail.com>
2022-03-17 14:34:54 +0100

    third commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 
-module.exports = withPlugins([optimizedImages])
+module.exports = {
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+  }
+// module.exports = withPlugins([optimizedImages])
